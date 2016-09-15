706129f39a1c3ad09b43a0275a1ba810a42403c8
stevellwood <ellwoodis@hotmail.com>
2016-09-15 10:49:33 -0400

    advanced complete

--- JDBCProject1/src/JDBCfiles/JDBCcommands.java
+++ JDBCProject1/src/JDBCfiles/JDBCcommands.java
@@ -117,13 +117,53 @@ public class JDBCcommands {
            System.out.println();
 
            // 4. Process the result set
-           if (rowAffected == 1) {
-               System.out.println("Row Affected by delete " + rowAffected + "named "+ last);
+           if (rs != null) {
+               
+               displayStudent(first,last);
+               System.out.println("Delete occurred at row" + rowAffected + " for student "+first +" "+last);
            } else {
-               System.out.println("Row Not Affected bye delete" + rowAffected);
+               System.out.println("No delete occurred" + rowAffected);
            }
 
        } catch (Exception exc) {
+           exc.printStackTrace();
+       } finally {
+           if (myConn != null) {
+               myConn.close();
+           }
+           if (stmt != null) {
+               stmt.close();
+           }
+       }
+   }
+   public void backup() throws SQLException {
+       try {
+           myConn = DriverManager.getConnection(JDBCconnect.thedburl,JDBCconnect.theuser,JDBCconnect.thepassword);
+           stmt = myConn.createStatement();             
+           //String sql = "delete from student where sat="+satInterimscore+" and"+ "last_name="+"'"+last+"'";
+           String sql = "select * from student;";
+           //int rowAffected = stmt.executeUpdate(sql);
+           System.out.println();
+           rs =stmt.executeQuery(sql);
+           System.out.println("Backup Resultset for recreating Table fields:First Name, Second Name, Major, Sat, GPA");
+           System.out.println("====================================================================================");
+           if (rs!=null){    
+           while(rs.next()){
+                   //System.out.println(rs.getString("id")+ "  "+rs.getString("first_name") + "\t\t"+ rs.getString("last_name")+ "\t"+rs.getString("major_id")+  " \t"+rs.getString("sat")+ "    "+rs.getString("gpa"));
+          String sql2 = "insert student (first_name,last_name,major_id,sat,gpa) values('George','Washington',null,1600,4.0);";
+           String start ="insert student (first_name,last_name,major_id,sat,gpa) values (";
+           int psat = Integer.parseInt(rs.getString("sat"));
+           double gpad = Double.parseDouble(rs.getString("gpa"));
+           //System.out.printf("%s %s,%s, %s, %d,%2.1f", start,rs.getString("first_name"),rs.getString("last_name"),rs.getString("major_id"),rs.getString("sat"),rs.getString("gpa"));
+           System.out.printf("%s'%s','%s','%s',%d,%2.2f);%n", start,rs.getString("first_name"),rs.getString("last_name"),rs.getString("major_id"),psat,gpad);
+                   //rs.getString("first_name"),rs.getString("last_name"),rs.getString("major_id"),rs.getString("sat"),rs.getString("gpa"));
+           //int rowAffected = stmt.executeQuery(sql);
+           // 4. Process the result set
+//           } else  {
+//               System.out.println("Query not performed");
+           }
+          }
+        } catch (Exception exc) {
            exc.printStackTrace();
        } finally {
            if (myConn != null) {
@@ -144,6 +184,8 @@ public class JDBCcommands {
        connection2.displayStudent(firstname,secondname);
      connection2.update(firstname,secondname,3.5);
        connection2.deleteData(firstname,secondname);
+       connection2.backup();
+
 
    }
 
